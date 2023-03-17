7964275ccf3e78c02f4f76aa6a030ff8c7d83211
Prince Anthony <tonygh2019@gmail.com>
2023-03-17 03:01:22 -0700

    stuffs

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+        char small_first_alphabet = 'a';
+	char capital_first_alphabet = 'A';
+
+        while(small_first_alphabet <= 'z')
+        {
+                putchar(small_first_alphabet++);
+        }
+
+	while(capital_first_alphabet <= 'Z')
+        {
+                putchar(capital_first_alphabet++);
+        }
+
+        putchar('\n');
+        return 0;
+}
+
+
+
+
+
+
+
