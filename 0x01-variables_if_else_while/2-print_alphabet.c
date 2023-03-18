0e2bb83964b4016ceb6a2c46b0021b1fcb91f0ee
Prince Anthony <tonygh2019@gmail.com>
2023-03-18 05:35:27 -0700

    dfbcujbsuidcb

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char first_alphabet = 'a';
-	
-	while(first_alphabet <= 'z')
+
+	while (first_alphabet <= 'z')
 	{
 		putchar(first_alphabet++);
 	}
-	
+
 	putchar('\n');
-	return 0;
+	return (0);
 }
+
