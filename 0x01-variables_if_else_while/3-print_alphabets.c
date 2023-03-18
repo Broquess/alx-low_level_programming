85e8e7ebd9a1b0dfc88ef084e7c77e78de5e9f5e
Prince Anthony <tonygh2019@gmail.com>
2023-03-18 05:40:00 -0700

    dbvudc

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,26 +2,18 @@
 
 int main(void)
 {
-        char small_first_alphabet = 'a';
-	char capital_first_alphabet = 'A';
+    char small_first_alphabet = 'a';
+    char capital_first_alphabet = 'A';
 
-        while(small_first_alphabet <= 'z')
-        {
-                putchar(small_first_alphabet++);
-        }
+    while (small_first_alphabet <= 'z') {
+        putchar(small_first_alphabet++);
+    }
 
-	while(capital_first_alphabet <= 'Z')
-        {
-                putchar(capital_first_alphabet++);
-        }
+    while (capital_first_alphabet <= 'Z') {
+        putchar(capital_first_alphabet++);
+    }
 
-        putchar('\n');
-        return 0;
+    putchar('\n');
+    return 0;
 }
-
-
-
-
-
-
 
